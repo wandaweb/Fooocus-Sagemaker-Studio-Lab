26eb34ab64d31ef8b3dd63fdeab1e2986af33997
Wanda <63283384+wandaweb@users.noreply.github.com>
2023-11-07 12:36:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-if ! test -d Fooocus
+if [ ! -d "Fooocus" ]
 then
   git clone https://github.com/lllyasviel/Fooocus.git
   #git clone --depth 1 --branch V2 https://github.com/lllyasviel/Fooocus.git
 fi
 cd Fooocus
 git pull
-cd ..
-if ! test -e ~/.conda/envs/fooocus
+if [ ! -L ~/.conda/envs/fooocus ]
 then
     ln -s /tmp/fooocus ~/.conda/envs/
 fi
 eval "$(conda shell.bash hook)"
-if ! test -d /tmp/fooocus
+if [ ! -d /tmp/fooocus ]
 then
     mkdir /tmp/fooocus
     conda env create -f environment.yaml
     conda activate fooocus
+    pwd
+    ls
     pip install -r requirements_versions.txt
     pip install torch torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu117
     pip install pyngrok
@@ -24,18 +25,11 @@ then
     rm -rf ~/.cache/pip
 fi
 conda activate fooocus
+cd ..
 if [ $# -eq 0 ]
 then
-  python Fooocus/entry_with_update.py & python start-ngrok.py  
+  python start-ngrok.py 
 elif [ $1 = "reset" ]
 then
-  python Fooocus/entry_with_update.py & python start-ngrok.py --reset
+  python start-ngrok.py --reset 
 fi
-
-
-
-
-
-
-
-
