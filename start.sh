592372becd16719d8a50437b5fdd6e24394b3129
Wanda <63283384+wandaweb@users.noreply.github.com>
2023-12-04 11:29:09 +0100

    replace checkpoints directory with checkpoints-real-folder

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ if [ ! -d "Fooocus" ]
 then
   git clone https://github.com/lllyasviel/Fooocus.git
   #git clone --depth 1 --branch V2 https://github.com/lllyasviel/Fooocus.git
+  # Create the config file pointing the checkpoints to checkpoints-real-folder
 fi
 cd Fooocus
 git pull
@@ -24,6 +25,28 @@ then
     conda install glib -y
     rm -rf ~/.cache/pip
 fi
+
+# Because the file manager in Sagemaker Studio Lab ignores the folder called "checkpoints"
+# we need to move checkpoint files into a folder with a different name
+current_folder=$(pwd)
+model_folder=${current_folder}/models/checkpoints-real-folder
+if [ ! -e config.txt ]
+then
+  json_data="{ \"path_checkpoints\": \"$model_folder\" }"
+  echo "$json_data" > config.txt
+  echo "JSON file created: config.txt"
+else
+  echo "Updating config.txt to use checkpoints-real-folder"
+  jq --arg new_value "$model_folder" '.path_checkpoints = $new_value' config.txt > config_tmp.txt && mv config_tmp.txt config.txt
+fi
+
+# If the checkpoints folder exists, move it to the new checkpoints-real-folder
+if [ ! -L models/checkpoints ]
+then
+    mv models/checkpoints models/checkpoints-real-folder
+    ln -s models/checkpoints-real-folder models/checkpoints
+fi
+
 conda activate fooocus
 cd ..
 if [ $# -eq 0 ]
