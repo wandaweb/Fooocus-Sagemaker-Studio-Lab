f2d323588ffd1a3ca865d84afa9ea5b9554bf4c9
Wanda <63283384+wandaweb@users.noreply.github.com>
2024-02-10 00:56:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set this variable to true to install to the temporary folder, or to false to have the installation in permanent storage.
-install_in_temp_dir=true
+install_in_temp_dir=false
 
 if [ ! -d "Fooocus" ]
 then
